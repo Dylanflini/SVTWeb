f55cdca6b485b35a372c6659c336cfbb3ea5cb3e
Pabluwu <lopez.pablo2305@gmail.com>
2020-04-11 03:09:49 -0400

    missing )

--- SVT/settings.py
+++ SVT/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
